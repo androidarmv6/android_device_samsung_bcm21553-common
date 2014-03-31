9882b0951a0740be714239572597c6ffa477616d
bieltv3 <bieltv.3@gmail.com>
2014-03-31 11:49:43 +0200

    Fix USB mass storage, new configs, improve performance

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,57 +20,60 @@
 
 LOCAL_PATH:= $(call my-dir)
 
-# Board
-TARGET_ARCH_VARIANT				:= armv6-vfp
-TARGET_CPU_ABI					:= armeabi-v6l
-TARGET_CPU_ABI2					:= armeabi
-TARGET_SPECIFIC_HEADER_PATH			:= device/samsung/bcm21553-common/include
+## Kernel
 TARGET_NO_BOOTLOADER				:= true
 TARGET_NO_RADIOIMAGE				:= true
-
 TARGET_PROVIDES_INIT				:= true
 TARGET_PROVIDES_INIT_TARGET_RC			:= true
 
-# Broadcom hardware drivers
+## Platform
+TARGET_BOARD_PLATFORM 				:= bcm21553
+TARGET_ARCH					:= arm
+TARGET_ARCH_VARIANT				:= armv6-vfp
+TARGET_CPU_ABI					:= armeabi-v6l
+TARGET_CPU_ABI2					:= armeabi
+#TARGET_BOARD_PLATFORM_GPU			:= videocoreIV
+TARGET_SPECIFIC_HEADER_PATH			:= device/samsung/bcm21553-common/include
+
+## Broadcom hardware drivers
 BCM21553_HARDWARE				:= true
 
-# TouchScreen
+## TouchScreen
 BOARD_USE_LEGACY_TOUCHSCREEN			:= true
 
-# Audio
+## Audio
 BOARD_USES_GENERIC_AUDIO			:= false
 BOARD_HAVE_SAMSUNG_AUDIO			:= true
 BOARD_USES_ALSA_AUDIO				:= true
 
-# RIL
+## RIL
 BOARD_USES_LEGACY_RIL				:= true
 BOARD_FORCE_RILD_AS_ROOT			:= true
 BOARD_MOBILEDATA_INTERFACE_NAME			:= "pdp0"
 
-# Camera
+## Camera
 USE_CAMERA_STUB					:= true
 BOARD_V4L2_DEVICE				:= /dev/video2
 BOARD_CAMERA_DEVICE				:= /dev/video0
 BOARD_USE_JPEG					:= true
 
-# 3D
+## 3D
 BOARD_EGL_CFG					:= device/samsung/bcm21553-common/prebuilt/lib/egl/egl.cfg
 BOARD_NO_RGBX_8888				:= true
 ENABLE_WEBGL					:= true
 
-# Browser / Stagefright
+## Browser / Stagefright
 JS_ENGINE 					:= v8
 HTTP 						:= chrome
 WITH_JIT					:= true
 ENABLE_JSC_JIT					:= true
 
-
-# USB
+## USB
 BOARD_USE_USB_MASS_STORAGE_SWITCH 		:= true
 TARGET_USE_CUSTOM_LUN_FILE_PATH 		:= "/sys/devices/lm-2/gadget/lun0/file"
 BOARD_UMS_LUNFILE 				:= "/sys/devices/lm-2/gadget/lun0/file"
 
-# Wifi
+## Wifi
 BOARD_WPA_SUPPLICANT_DRIVER			:= WEXT
 WPA_SUPPLICANT_VERSION				:= VER_0_6_X
 WIFI_DRIVER_MODULE_PATH				:= "/system/lib/modules/dhd.ko"
@@ -79,11 +82,16 @@ WIFI_DRIVER_FW_AP_PATH				:= "/system/etc/wifi/bcm4330_aps.bin"
 WIFI_DRIVER_MODULE_ARG				:= "firmware_path=/system/etc/wifi/bcm4330_sta.bin nvram_path=/system/etc/wifi/nvram.txt"
 WIFI_DRIVER_MODULE_NAME				:= "bcm4330"
 
-# Bluetooth
+## Wi-Fi Hotspot
+BOARD_HAVE_LEGACY_HOSTAPD			:= true
+BOARD_HOSTAPD_NO_ENTROPY			:= true
+
+## Bluetooth
 BOARD_HAVE_BLUETOOTH				:= true
 BOARD_HAVE_BLUETOOTH_BCM			:= true
+BOARD_HAVE_SAMSUNG_BLUETOOTH			:= true
 
-# FM
+## FM
 # BOARD_HAVE_FM_RADIO				:= true
 # BOARD_GLOBAL_CFLAGS				+= -DHAVE_FM_RADIO
 # BOARD_FM_DEVICE				:= bcm4329
--- common.mk
+++ common.mk
@@ -40,19 +40,19 @@ PRODUCT_PACKAGES += \
 #    audio.a2dp.default \
 #    SoundRecoder
 
-# Video decoding
+## Video decoding
 PRODUCT_PACKAGES += \
     libstagefrighthw \
     libopencorehw \
     libmm-omxcore \
     libOmxCore
 
-# Other
+## Other
 PRODUCT_PACKAGES += \
     FileManager \
     setup_fs
 
-# These are the hardware-specific features
+## These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
@@ -68,7 +68,7 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
-# Keylayout
+## Keylayout
 PRODUCT_COPY_FILES += \
     device/samsung/bcm21553-common/prebuilt/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
     device/samsung/bcm21553-common/prebuilt/usr/keylayout/bcm_headset.kl:system/usr/keylayout/bcm_headset.kl \
@@ -77,72 +77,54 @@ PRODUCT_COPY_FILES += \
     device/samsung/bcm21553-common/prebuilt/usr/keylayout/sec_key.kl:system/usr/keylayout/sec_key.kl \
     device/samsung/bcm21553-common/prebuilt/usr/keylayout/sec_keypad.kl:system/usr/keylayout/sec_keypad.kl
 
-# Keychars
+## Keychars
 PRODUCT_COPY_FILES += \
     device/samsung/bcm21553-common/prebuilt/usr/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
     device/samsung/bcm21553-common/prebuilt/usr/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin \
     device/samsung/bcm21553-common/prebuilt/usr/keychars/sec_key.kcm.bin:system/usr/keychars/sec_key.kcm.bin
 
-# Media profiles
+## Media
 #PRODUCT_COPY_FILES += \
 #    device/samsung/bcm21553-common/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml
+#    device/samsung/bcm21553-common/prebuilt/etc/asound.conf:system/etc/asound.conf
 
-# HW Drivers
+## HW Drivers
 PRODUCT_COPY_FILES += \
     device/samsung/bcm21553-common/prebuilt/lib/egl/egl.cfg:system/lib/egl/egl.cfg
 
-# WIFI
+## WIFI
 PRODUCT_COPY_FILES += \
-    device/samsung/bcm21553-common/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+    device/samsung/bcm21553-common/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    device/samsung/bcm21553-common/prebuilt/bin/get_macaddrs:system/bin/get_macaddrs
 
-# New SamsungRIL
+## Graphics properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.hw=1 \
+    debug.enabletr=false \
+    debug.composition.type=mdp \
+    debug.gr.numframebuffers=2 \
+    debug.qctwa.statusbar=1 \
+    debug.qctwa.preservebuf=1 \
+    debug.performance.tuning=1 \
+    hwui.render_dirty_regions=false \
+    hwui.disable_vsync=true \
+    hwui.print_config=choice \
+    persist.sys.strictmode.visual=false \
+    persist.sys.use_dithering=1
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    video.accelerate.hw=1 \
+    media.stagefright.enable-player=true \
+    media.stagefright.enable-meta=false \
+    media.stagefright.enable-scan=false \
+    media.stagefright.enable-http=true \
+    media.stagefright.enable-aac=true \
+    media.stagefright.enable-qcp=true
+
+## Samsung RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=SamsungRIL \
     mobiledata.interfaces=pdp0,eth0,gprs,ppp0
 
-# Performance & graphics properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapsize=32m \
-    debug.camcorder.disablemeta=1 \
-    debug.enabletr=false \
-    hwui.render_dirty_regions=false \
-    persist.sys.purgeable_assets=1 \
-    persist.sys.ui.hw=1 \
-    persist.sys.use_dithering=1 \
-    ro.hwui.texture_cache_size=12 \
-    ro.hwui.layer_cache_size=8 \
-    ro.hwui.gradient_cache_size=0.25 \
-    ro.hwui.path_cache_size=2 \
-    ro.hwui.shape_cache_size=0.5 \
-    ro.hwui.drop_shadow_cache_size=1 \
-    ro.hwui.fbo_cache_size=8 \
-    ro.media.dec.jpeg.memcap=20000000 \
-    ro.opengles.version=131072 \
-    ro.sf.lcd_density=160 \
-    ro.vold.umsdirtyratio=20
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dexopt-data-only=1
-
-# Extended JNI checks
-# The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs 
-# before they have a chance to cause problems.
-# Default=true for development builds, set by android buildsystem.
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.kernel.android.checkjni=0 \
-    dalvik.vm.checkjni=false
-
-# We have enough storage space to hold precise GC data
+## We have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
-
-# NEW ICS properties (may need verification/testing)
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config_datause_iface=pdp0 \
-    ro.secure=0 \
-    ro.ril.hsxpa=1 \
-    ro.ril.gprsclass=10 \
-    ro.telephony.ril.v3=icccardstatus,datacall,signalstrength,facilitylock \
-    debug.gr.swapinterval=0 \
-    persist.sys.usb.config=mass_storage,adb \
-    sys.usb.config=mass_storage,adb \
-    persist.service.adb.enable=1
