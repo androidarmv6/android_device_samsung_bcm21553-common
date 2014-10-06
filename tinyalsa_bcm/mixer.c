eb32975071ab927f3356ecf8e76fc92cf5254805
Conn O'Griofa <connogriofa@gmail.com>
2014-10-06 03:48:56 +0100

    audio: major routing improvements
    * tinyalsa_bcm: update & implement support for subdevice selection
    * implement proper audio routing via ALSA subdevice selection &
      enabling of telephony path via PCG_IOCTL_SETMODEAPP ioctl.
      This eliminates popping noises, and prevents unwanted routing back
      to the speaker under certain circumstances.
    * Avoid buffer overruns if pcm_get_timestamp fails (reproducible by
      enabling wifi while playing audio on the headset).
    * implement correct SCO (BT headset) routing.
    
    Change-Id: I34a3d1cba5bc7654c8cdaf31723356cf7c5dbec9

--- tinyalsa_bcm/mixer.c
+++ tinyalsa_bcm/mixer.c
@@ -383,20 +383,12 @@ int mixer_ctl_set_value(struct mixer_ctl *ctl, unsigned int id, int value)
     if (ret < 0)
         return ret;
 
-    if (value > mixer_ctl_get_range_max(ctl))
-        value = mixer_ctl_get_range_max(ctl);
-
     switch (ctl->info->type) {
     case SNDRV_CTL_ELEM_TYPE_BOOLEAN:
         ev.value.integer.value[id] = !!value;
         break;
 
     case SNDRV_CTL_ELEM_TYPE_INTEGER:
-        if ((value < mixer_ctl_get_range_min(ctl)) ||
-            (value > mixer_ctl_get_range_max(ctl))) {
-            return -EINVAL;
-        }
-
         ev.value.integer.value[id] = value;
         break;
 
