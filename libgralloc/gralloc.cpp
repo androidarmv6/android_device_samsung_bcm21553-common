d66f76490a2314c08b09a8978b1f8c3954d38409
Conn O'Griofa <connogriofa@gmail.com>
2014-12-14 10:16:16 +0000

    bcm21553-common: initial cm-12.0 bringup
    * Enable TARGET_USERIMAGES_USE_EXT4 (to avoid build error)
    * Update bcm21553-bootimg.mk for Lollipop
    * Bluedroid: enable BLE, GATT, SMP (to avoid build error)
    * overlay: remove config_samsung_stk bool
    * Switch to dlmalloc implementation (to fix build error)
    * Update sepolicy file_contexts for /sd-ext
    * Install cm-11.0 android_pmem.h bionic/libc header (to fix
      libbinder build)
    * Implement camera parameters via CameraParametersExtra.h, fix
      dlopen code & remove max saturation/contrast/sharpness from HAL
    * Update libaudio (from grouper lollipop version)
    * Update libgralloc
    * Split off fstab into boot, cm-recovery and twrp-recovery variants
    
    Change-Id: I472ee1d7581be3db178c5181e3d79318e0e6452c

--- libgralloc/gralloc.cpp
+++ libgralloc/gralloc.cpp
@@ -86,31 +86,31 @@ extern int gralloc_unregister_buffer(gralloc_module_t const* module,
 /*****************************************************************************/
 
 static struct hw_module_methods_t gralloc_module_methods = {
-        open: gralloc_device_open
+        .open = gralloc_device_open
 };
 
 struct private_module_t HAL_MODULE_INFO_SYM = {
-    base: {
-        common: {
-            tag: HARDWARE_MODULE_TAG,
-            version_major: 1,
-            version_minor: 0,
-            id: GRALLOC_HARDWARE_MODULE_ID,
-            name: "Graphics Memory Allocator Module",
-            author: "The Android Open Source Project",
-            methods: &gralloc_module_methods
+    .base = {
+        .common = {
+            .tag = HARDWARE_MODULE_TAG,
+            .version_major = 1,
+            .version_minor = 0,
+            .id = GRALLOC_HARDWARE_MODULE_ID,
+            .name = "Graphics Memory Allocator Module",
+            .author = "The Android Open Source Project",
+            .methods = &gralloc_module_methods
         },
-        registerBuffer: gralloc_register_buffer,
-        unregisterBuffer: gralloc_unregister_buffer,
-        lock: gralloc_lock,
-        unlock: gralloc_unlock,
+        .registerBuffer = gralloc_register_buffer,
+        .unregisterBuffer = gralloc_unregister_buffer,
+        .lock = gralloc_lock,
+        .unlock = gralloc_unlock,
     },
-    framebuffer: 0,
-    flags: 0,
-    numBuffers: 0,
-    bufferMask: 0,
-    lock: PTHREAD_MUTEX_INITIALIZER,
-    currentBuffer: 0,
+    .framebuffer = 0,
+    .flags = 0,
+    .numBuffers = 0,
+    .bufferMask = 0,
+    .lock = PTHREAD_MUTEX_INITIALIZER,
+    .currentBuffer = 0,
 };
 
 /*****************************************************************************/
@@ -211,7 +211,7 @@ static int gralloc_alloc_framebuffer(alloc_device_t* dev,
 
 #ifndef BCM_HARDWARE
 static int gralloc_alloc_buffer(alloc_device_t* dev,
-        size_t size, int usage, buffer_handle_t* pHandle)
+        size_t size, int /*usage*/, buffer_handle_t* pHandle)
 {
     int err = 0;
     int fd = -1;
@@ -240,7 +240,7 @@ static int gralloc_alloc_buffer(alloc_device_t* dev,
 }
 #else
 static int gralloc_alloc_buffer(alloc_device_t* dev,
-        size_t size, int usage, buffer_handle_t* pHandle,int w,int h,int format,int stride, int hstride)
+        size_t size, int /*usage*/, buffer_handle_t* pHandle,int w,int h,int format,int stride, int hstride)
 {
     int pgsize = PAGE_SIZE;
     gralloc_context_t* ctx = (gralloc_context_t*)dev;
